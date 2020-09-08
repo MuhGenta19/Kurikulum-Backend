43f669c6e58a736fc29dfeef14623f6ae8ad4605
MuhGenta19 <muhammadgentaalam@gmail.com>
2020-09-08 21:38:12 +0700

    tugas

---
+++ sprint-1/topik-3/tugas-3.2/latihan1-3.2.php
@@ -0,0 +1,17 @@
+<?php
+
+for ($i = 1; $i <= 9; $i++) {
+    for ($j = 1; $j <= 9; $j++) {
+        if ($i == $j || $i > $j) {
+            echo "&nbsp&nbsp+";
+        } else {
+            echo "&nbsp&nbsp-&nbsp";
+
+        }
+    }
+    echo "<br>";
+}
+
+
+
+?>
---
+++ sprint-1/topik-3/tugas-3.2/latihan3-3.2.php
@@ -0,0 +1,15 @@
+<?php
+
+for ($i = 1; $i <= 9; $i++) {
+    for ($j = 1; $j <= 9; $j++) {
+        if ($i + $j == 10 || $i == $j || $i > $j && $i + $j > 10 || $i < $j && $i + $j < 10) {
+            echo "&nbsp+&nbsp";
+        } else {
+            echo "&nbsp&nbsp-&nbsp";
+
+        }
+    }
+    echo "<br>";
+}
+
+?>
