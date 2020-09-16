c5c1abdd1c4e98620376657802a88b6cddb43f82
MuhGenta19 <muhammadgentaalam@gmail.com>
2020-09-17 05:46:52 +0700

    tugas

---
+++ sprint-2/topik-3/tugas-3.0/latihan1-2.0.php
@@ -0,0 +1,80 @@
+<?php
+
+class RumusLuas
+{
+    public static function calcRectangleArea($lebar, $panjang) {
+        if ($lebar < 0 || $panjang < 0) {
+            throw new Exception('lebar atau panjang tidak boleh 0 !'."\n");
+        }
+        $hasil = $lebar*$panjang;
+        echo $hasil;
+        echo "\n";
+    }
+    
+    public static function calcCircleArea($jari2) {
+        if ($jari2 < 0) {
+            throw new Exception('jari-jari tidak boleh 0 !'."\n");
+        }
+        $hasil = 22/7*($jari2*$jari2);
+        echo $hasil;
+        echo "\n";
+    }
+    
+    public static function calcTriangleArea($alas, $tinggi) {
+        if ($alas < 0 || $tinggi < 0) {
+            throw new Exception('alas atau tinggi tidak boleh 0 !'."\n");
+        }
+        $hasil = 1/2*$alas*$tinggi;
+        echo $hasil;
+        echo "\n";
+    }
+    
+    public static function calcTrapesiumArea($sisi1, $sisi2, $tinggi) {
+        if ($sisi1 < 0 || $sisi2 < 0 || $tinggi < 0) {
+            throw new Exception('sisi atau tinggi tidak boleh 0 !'."\n");
+        }
+        $hasil = 1/2*($sisi1+$sisi2)*$tinggi;
+        echo $hasil;
+        echo "\n";
+    }
+}
+
+try {
+    RumusLuas::calcRectangleArea(4, -8);
+} catch (Exception $ex) {
+    // var_dump($ex);
+    echo "Error pada ".$ex->getFile()." baris ke : ". $ex->getLine()."\n";
+    echo $ex->getMessage();
+    echo "\n";
+}
+
+try {
+    RumusLuas::calcCircleArea(-4.5);
+} catch (Exception $ex) {
+    // var_dump($ex);
+    echo "Error pada ".$ex->getFile()." baris ke : ". $ex->getLine()."\n";
+    echo $ex->getMessage();
+    echo "\n";
+}
+
+try {
+    RumusLuas::calcTriangleArea(5, -4);
+} catch (Exception $ex) {
+    // var_dump($ex);
+    echo "Error pada ".$ex->getFile()." baris ke : ". $ex->getLine()."\n";
+    echo $ex->getMessage();
+    echo "\n";
+}
+
+try {
+    RumusLuas::calcTrapesiumArea(-4, 10, 5);
+} catch (Exception $ex) {
+    // var_dump($ex);
+    echo "Error pada ".$ex->getFile()." baris ke : ". $ex->getLine()."\n";
+    echo $ex->getMessage();
+    echo "\n";
+}
+RumusLuas::calcRectangleArea(4, 8);
+RumusLuas::calcCircleArea(4.5);
+RumusLuas::calcTriangleArea(5, 4);
+RumusLuas::calcTrapesiumArea(4, 10, 5);
